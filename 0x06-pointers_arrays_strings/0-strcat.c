63a54e638edf95b5e7d5922559e0d063fc07fbf8
Bongnkosi <comediandrshakes@gmail.com>
2023-09-20 08:21:19 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include <stdio.h>
+
 
 /**
  *_strcat - Write a function that concatenates two strings.
@@ -7,7 +8,7 @@
  *@src: This is the input source
  *
  * Return: This return to dest, that concatenates two strings
- */
+*/
 
 char *_strcat(char *dest, char *src)
 {
